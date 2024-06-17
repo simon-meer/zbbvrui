ddc451ec5ef3514f74ea9809bb87006151adf18b
u222761 <simon.meer@sbb.ch>
2024-06-17 16:40:23 +0200

    fix: fix drifting window position on Windows

--- window_manager/src/windows.rs
+++ window_manager/src/windows.rs
@@ -1,10 +1,28 @@
 use std::mem;
-use windows_sys::Win32::Foundation::{HWND, RECT};
-use windows_sys::Win32::UI::WindowsAndMessaging::{
-    EnumWindows, GetWindowRect, GetWindowThreadProcessId, SetWindowPos, SWP_NOZORDER,
-};
+use windows_sys::Win32::Foundation::{HWND, POINT, RECT};
+use windows_sys::Win32::UI::WindowsAndMessaging::{EnumWindows, GetClientRect, GetWindowPlacement, GetWindowRect, GetWindowThreadProcessId, SetWindowPos, SW_SHOWMINIMIZED, SWP_NOZORDER, WINDOWPLACEMENT};
 use crate::{Position, WindowError};
-use std::ptr::{null, null_mut};
+
+const DEFAULT_WINDOWPLACEMENT: WINDOWPLACEMENT = WINDOWPLACEMENT {
+    length: 0,
+    flags: 0,
+    showCmd: 0,
+    ptMinPosition: POINT { x: 0, y: 0},
+    ptMaxPosition: POINT { x: 0, y: 0},
+    rcNormalPosition: RECT {
+        left: 0,
+        top: 0,
+        right: 0,
+        bottom: 0,
+    },
+};
+
+const DEFAULT_RECT: RECT = RECT  {
+    left: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
+};
 
 pub fn get_window_position(pid: u32) -> Result<Position, WindowError> {
     let hwnd = find_window_by_pid(pid).ok_or(WindowError::NotFound)?;
@@ -61,15 +79,29 @@ unsafe extern "system" fn enum_windows_proc(hwnd: HWND, lparam: isize) -> i32 {
 }
 
 fn get_window_rect(hwnd: HWND) -> Result<RECT, WindowError> {
-    let mut rect = RECT {
-        left: 0,
-        top: 0,
-        right: 0,
-        bottom: 0,
-    };
+    let mut win_rect = DEFAULT_RECT.clone();
+    let mut client_rect = DEFAULT_RECT.clone();
+    let mut window_placement = DEFAULT_WINDOWPLACEMENT.clone();
+
     unsafe {
-        if GetWindowRect(hwnd, &mut rect) != 0 {
-            Ok(rect)
+        if GetWindowPlacement(hwnd, &mut window_placement) == 0 {
+            return Err(WindowError::Other("Failed to get window placement".to_string()));
+        }
+        
+        if window_placement.showCmd == SW_SHOWMINIMIZED as u32 {
+            return Err(WindowError::Other("Window is minimized".to_string()));
+        }
+        
+        if GetClientRect(hwnd, &mut client_rect) != 0 && GetWindowRect(hwnd, &mut win_rect) != 0 {
+            let border_width = ((win_rect.right - win_rect.left) - client_rect.right) / 2;
+            let header_height = ((win_rect.bottom - win_rect.top) - client_rect.bottom) - border_width;
+            
+            Ok(RECT {
+                left: win_rect.left + border_width,
+                right: win_rect.right - border_width,
+                top: win_rect.top + header_height,
+                bottom: win_rect.bottom - border_width
+            })
         } else {
             Err(WindowError::Other("Failed to get window rect".to_string()))
         }
