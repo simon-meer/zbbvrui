e6bef0d10fc72a0fec942413cba8de0e2dc2883a
u222761 <simon.meer@sbb.ch>
2024-07-01 16:18:03 +0200

    fix: show icons when there is no internet

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
-import { bootstrapApplication } from "@angular/platform-browser";
-import { appConfig } from "./app/app.config";
-import { AppComponent } from "./app/app.component";
+import {bootstrapApplication} from "@angular/platform-browser";
+import {appConfig} from "./app/app.config";
+import {AppComponent} from "./app/app.component";
+import {mergeConfig} from "@sbb-esta/lyne-elements/core/config.js";
+
+mergeConfig({
+    icon: {
+        namespaces: new Map<string, string>().set("default", "icons/")
+    }
+})
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err),
+    console.error(err),
 );
